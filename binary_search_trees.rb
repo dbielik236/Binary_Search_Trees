fe04c805cc7d97f8ad10d5bbe2592b96943b01c4
David Bielik <davidbielik@gmail.com>
2022-03-15 09:36:31 -0400

    initial structure of classes

---
+++ binary_search_trees.rb
@@ -0,0 +1,22 @@
+class Node
+    attr_accessor :data, :left_child :right_child
+end
+  
+module Comparable
+end
+  
+class Tree
+  attr_accessor :root
+  
+  def initialize(array)
+    @array=array
+
+    #merge_sort the array
+    #remove any repeats
+    
+  end
+
+  def build_tree
+  end
+  
+end
